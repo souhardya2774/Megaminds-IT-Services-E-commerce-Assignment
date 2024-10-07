4d4e30381e0eeba2dabeb77bb5752c3553b5e136
souhardya2774 <souhardya2774@gmail.com>
2024-10-07 17:17:05 +0530

    Basic code up with cors1

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,6 +3,7 @@ import MaxWidthWrapper from './MaxWidthWrapper'
 import { buttonVariants } from './ui/button'
 import { ArrowRight } from 'lucide-react'
 import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
+import {RegisterLink, LoginLink, LogoutLink} from "@kinde-oss/kinde-auth-nextjs/components";
 
 const Navbar = async () => {
   const { getUser } = getKindeServerSession()
@@ -21,14 +22,13 @@ const Navbar = async () => {
           <div className='h-full flex items-center space-x-4'>
             {user ? (
               <>
-                <Link
-                  href='/api/auth/logout'
+                <LogoutLink
                   className={buttonVariants({
                     size: 'lg',
                     variant: 'ghost',
                   })}>
                   Sign out
-                </Link>
+                </LogoutLink>
                 {isAdmin && (
                   <Link
                     href='/dashboard'
@@ -51,23 +51,22 @@ const Navbar = async () => {
               </>
             ) : (
               <>
-                <Link
-                  href='/api/auth/register'
+                <RegisterLink
                   className={buttonVariants({
                     size: 'lg',
                     variant: 'ghost',
                   })}>
                   Sign up
-                </Link>
+                </RegisterLink>
 
-                <Link
+                <LoginLink
                   href='/api/auth/login'
                   className={buttonVariants({
                     size: 'lg',
                     variant: 'ghost',
                   })}>
                   Login
-                </Link>
+                </LoginLink>
 
                 <div className='h-8 w-px bg-zinc-200 hidden lg:block' />
 

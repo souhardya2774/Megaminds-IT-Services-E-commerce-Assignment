4b45682d39266897d30692d3497b1af638349c61
souhardya2774 <souhardya2774@gmail.com>
2024-10-07 16:52:04 +0530

    Basic code up with cors1

--- middleware.ts
+++ middleware.ts
@@ -14,6 +14,7 @@ export function middleware(request: NextRequest) {
 
   const origin = process.env.ALLOWED_ORIGIN || '*'
   response.headers.set('Access-Control-Allow-Origin', origin)
+  response.headers.set('Access-Control-Allow-Credentials', 'true')
   response.headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
   response.headers.set('Access-Control-Allow-Headers', 'Content-Type, Authorization')
   response.headers.set('Access-Control-Max-Age', '86400')

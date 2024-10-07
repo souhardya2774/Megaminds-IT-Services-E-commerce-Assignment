95070f9ca5d982b99f3030df2ec3acd3b39fa794
souhardya2774 <souhardya2774@gmail.com>
2024-10-07 15:59:01 +0530

    Basic code up with cors

---
+++ middleware.ts
@@ -0,0 +1,34 @@
+import { NextRequest, NextResponse } from "next/server";
+
+const allowedOrigins = [
+    process.env.NEXT_PUBLIC_SERVER_URL
+];
+
+export function middleware(req:NextRequest) {
+    // retrieve the current response
+    const res = NextResponse.next()
+
+    // retrieve the HTTP "Origin" header 
+    // from the incoming request
+    req.headers.get("origin")
+
+    // if the origin is an allowed one,
+    // add it to the 'Access-Control-Allow-Origin' header
+    if (allowedOrigins.includes(origin)) {
+      res.headers.append('Access-Control-Allow-Origin', origin);
+    }
+
+    // add the remaining CORS headers to the response
+    res.headers.append('Access-Control-Allow-Credentials', "true")
+    res.headers.append('Access-Control-Allow-Methods', 'GET,DELETE,PATCH,POST,PUT')
+    res.headers.append(
+        'Access-Control-Allow-Headers',
+        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+    )
+
+    return res
+}
+
+export const config = {
+    matcher: '*',
+}
